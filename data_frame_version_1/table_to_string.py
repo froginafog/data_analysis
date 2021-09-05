0f82583c7b6389172f5342bfb4b20122de5761a1
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-09-05 16:05:12 -0400

    Add files via upload

---
+++ data_frame_version_1/table_to_string.py
@@ -0,0 +1,45 @@
+def table_to_string(column_names, table, starting_row, ending_row, starting_column, ending_column):
+    num_rows = len(table)
+    temp_table = []
+    for i in range(0, num_rows):
+        temp_table.append(table[i].copy())
+    max_column_lengths = [0] * len(column_names)
+    for j in range(starting_column, ending_column):
+        max_column_lengths[j] = len(column_names[j])
+    for i in range(starting_row, ending_row):
+        for j in range(starting_column, ending_column):
+            temp_table[i][j] = str(temp_table[i][j])
+            if(len(temp_table[i][j]) > max_column_lengths[j]):
+                max_column_lengths[j] = len(temp_table[i][j])
+    num_dashes = 0
+    for n in range(starting_column, ending_column):
+        num_dashes = num_dashes + max_column_lengths[n] + 3
+    num_dashes = num_dashes + 1
+    dashes = ""
+    for n in range(0, num_dashes):
+        dashes = dashes + '-'
+    output = dashes + "\n|" 
+    for j in range(starting_column, ending_column):
+        s = " %" + str(max_column_lengths[j]) + "s "
+        s = s % column_names[j]
+        output = output + s + "|"
+    output = output + "\n" + dashes + "\n"
+    for i in range(starting_row, ending_row):
+        output = output + dashes + "\n|"
+        for j in range(starting_column, ending_column):
+            s = " %" + str(max_column_lengths[j]) + "s "
+            s = s % temp_table[i][j]
+            output = output + s + "|"
+        output = output + "\n"
+    output = output + dashes + "\n"
+    return output
+
+column_names = ["column_1", "column_2", "column_3"]
+table = [[11, 12, 13],
+         [21, 22, 23],
+         [31, 32, 33]]
+NUM_ROWS = len(table)
+NUM_COLUMNS = len(table[0])
+table_string = table_to_string(column_names, table, 0, NUM_ROWS, 0, NUM_COLUMNS)
+print(table_string)
+
---
+++ data_frame_version_2/main.py
@@ -0,0 +1,106 @@
+import pandas
+
+def table_to_string(table_name, row_names, column_names, matrix):
+    num_rows = len(row_names)
+    column_0_width_max = len(table_name)
+    for i in range(0, num_rows):
+        width = len(row_names[i])
+        if(width > column_0_width_max):
+            column_0_width_max = width     
+    temp_matrix = []
+    for i in range(0, num_rows):
+        temp_matrix.append(matrix[i].copy())
+    column_widths_max = []
+    num_columns = len(column_names)
+    for j in range(0, num_columns):
+        column_widths_max.append(len(column_names[j]))  
+    for i in range(0, num_rows):
+        for j in range(0, num_columns):
+            temp_matrix[i][j] = str(temp_matrix[i][j])
+            if(len(temp_matrix[i][j]) > column_widths_max[j]):
+                column_widths_max[j] = len(temp_matrix[i][j])
+    num_dashes = column_0_width_max + 4
+    dashes_for_column_0 = ""
+    for n in range(0, num_dashes):
+        dashes_for_column_0 = dashes_for_column_0 + '-'
+    output = dashes_for_column_0 + " "
+    num_dashes = 0
+    for n in range(0, num_columns):
+        num_dashes = num_dashes + column_widths_max[n] + 3
+    num_dashes += 1
+    dashes = ""
+    for n in range(0, num_dashes):
+        dashes = dashes + '-'
+    output = output + dashes + "\n|"
+    s = " %" + str(column_0_width_max) + "s "
+    s = s % table_name
+    output = output + s + "| |"
+    for j in range(0, num_columns):
+        s = " %" + str(column_widths_max[j]) + "s "
+        s = s % column_names[j]
+        output = output + s + "|"
+    output = output + "\n" + dashes_for_column_0 + " " + dashes + "\n" + dashes_for_column_0 + " " + dashes + "\n"
+    for i in range(0, num_rows):
+        output = output + "|"
+        s = " %" + str(column_0_width_max) + "s "
+        s = s % row_names[i]
+        output = output + s + "| |"
+        for j in range(0, num_columns):
+            s = " %" + str(column_widths_max[j]) + "s "
+            s = s % temp_matrix[i][j]
+            output = output + s + "|"
+        output = output + "\n" + dashes_for_column_0 + " " + dashes + "\n"
+    return output
+
+def csv_to_pandas(filepath):
+    df = pandas.read_csv(filepath)
+    pandas.options.display.max_rows = None
+    pandas.options.display.max_columns = None
+    return df
+
+def pandas_to_table(df):
+    table_name = df.columns[0]
+    num_rows = df.shape[0]
+    row_names = []
+    for i in range(0, num_rows):
+        row_names.append(df.loc[i][0])
+    num_columns = df.shape[1]
+    temp_column_names = df.columns.tolist()
+    column_names = []
+    for j in range(0, num_columns - 1):
+        column_names.append(temp_column_names[j + 1])
+    temp_matrix = df.values.tolist()
+    matrix = []
+    for i in range(0, num_rows):
+        row = []
+        for j in range(1, num_columns):
+            row.append(temp_matrix[i][j])
+        matrix.append(row)
+    return table_name, row_names, column_names, matrix
+
+filepath = "table_data.csv"
+df = csv_to_pandas(filepath)
+#print("pandas dataframe:")
+#print(df)
+#print()
+
+table_name, row_names, column_names, matrix = pandas_to_table(df)
+
+print("table_name:", table_name)
+print()
+
+print("row_names:", row_names)
+print()
+
+print("column_names:", column_names)
+print()
+
+print("matrix:", matrix)
+print()
+
+table_string = table_to_string(table_name, row_names, column_names, matrix)
+
+print("table_string:")
+print(table_string)
+print()
+    
