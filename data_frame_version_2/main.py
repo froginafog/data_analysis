7f870bdd64bd075d8344476c2c0e3705d4a1b23c
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-09-07 15:51:08 -0400

    Add files via upload

--- data_frame_version_2/main.py
+++ data_frame_version_2/main.py
@@ -1,20 +1,33 @@
-#author: froginafog (Liang D.S.)
 import pandas
 
 def table_to_string(table_name, row_names, column_names, matrix):
-    num_rows = len(row_names)
+    row_names_copy = row_names.copy()
+    num_rows = len(row_names_copy)
+    for i in range(0, num_rows):
+        if(type(row_names_copy[i]) != str and type(row_names_copy[i]) != int):
+            integer, fraction = divmod(row_names_copy[i], 1)
+            if fraction == 0:
+                row_names_copy[i] = int(integer)
+    #------------------------------------------------------            
+    num_columns = len(column_names)
+    column_names_copy = column_names.copy()
+    for j in range(0, num_columns):
+        if(type(column_names_copy[j]) != str and type(column_names_copy[j]) != int):
+            integer, fraction = divmod(column_names_copy[j], 1)
+            if fraction == 0:
+                column_names_copy[j] = int(integer)
+    #------------------------------------------------------ 
     column_0_width_max = len(table_name)
     for i in range(0, num_rows):
-        width = len(row_names[i])
+        width = len(str(row_names_copy[i]))
         if(width > column_0_width_max):
             column_0_width_max = width     
     temp_matrix = []
     for i in range(0, num_rows):
         temp_matrix.append(matrix[i].copy())
     column_widths_max = []
-    num_columns = len(column_names)
     for j in range(0, num_columns):
-        column_widths_max.append(len(column_names[j]))  
+        column_widths_max.append(len(str(column_names_copy[j])))  
     for i in range(0, num_rows):
         for j in range(0, num_columns):
             temp_matrix[i][j] = str(temp_matrix[i][j])
@@ -38,13 +51,13 @@ def table_to_string(table_name, row_names, column_names, matrix):
     output = output + s + "| |"
     for j in range(0, num_columns):
         s = " %" + str(column_widths_max[j]) + "s "
-        s = s % column_names[j]
+        s = s % column_names_copy[j]
         output = output + s + "|"
     output = output + "\n" + dashes_for_column_0 + " " + dashes + "\n" + dashes_for_column_0 + " " + dashes + "\n"
     for i in range(0, num_rows):
         output = output + "|"
         s = " %" + str(column_0_width_max) + "s "
-        s = s % row_names[i]
+        s = s % row_names_copy[i]
         output = output + s + "| |"
         for j in range(0, num_columns):
             s = " %" + str(column_widths_max[j]) + "s "
